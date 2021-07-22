a7ab5085084e6686c281bf4770da32fe94a7d70f
Juan José Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-07-21 21:56:21 -0500

    Initial Commit

---
+++ opengl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opengl
+//
+//  Created by Juanjo Jaramillo on 21/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
