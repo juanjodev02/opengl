f366465fdbb116047bae2fc7fa6858303a11274b
Juan José Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-07-21 22:17:20 -0500

    implement tangram figure with solid colors

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -1,64 +1,144 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <shader_s.h>
+
 #include <iostream>
+#include <filesystem>
+#include <math.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-    GLFWwindow* window;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Initialize the library
-    if(!glfwInit())
-        return -1;
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
 
-    // Define version and compatibility settings
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    // Create a windowed mode window and its OpenGL context
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    // Mathe the window's context current
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Juan Jaramillo", NULL, NULL);
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // Initialize the OpenGL API with GLAD
+    
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // Loop until the user closes the window
-    while(!glfwWindowShouldClose(window))
+    Shader ourShader("shaders/practica_shader_class.vs", "shaders/practica_shader_class.fs");
+    
+    //Iniciamos los vertices
+    float vertices[] = {
+        //triangle 1
+        // positions         // colors
+        0.0f, 0.5f, 0.0f,  0.2f, 0.6f, 1.0f,  // bottom right
+        -0.2f, 0.3f, 0.0f,  0.2f, 0.6f, 1.0f,  // bottom left
+        0.0f, 0.3f, 0.0f,  0.2f, 0.6f, 1.0f,   // top
+        //triangle 2
+        // positions         // colors
+        0.0f, 0.3f, 0.0f,  0.2f, 0.8f, 0.4f,  // bottom right
+        -0.2f, 0.0f, 0.0f,  0.2f, 0.8f, 0.4f,  // bottom left
+        0.0f, -0.3f, 0.0f,  0.2f, 0.8f, 0.4f,   // top
+        //triangle 3
+        // positions         // colors
+        0.0f, 0.2f, 0.0f,  1.0f, 0.8f, 0.0f,  // bottom right
+        0.0f, -0.25f, 0.0f,  1.0f, 0.8f, 0.0f,  // bottom left
+        0.3f, -0.25f, 0.0f,  1.0f, 0.8f, 0.0f,   // top
+        //triangle 4
+        // positions         // colors
+        -0.2f, 0.0f, 0.0f,  1.0f, 0.4f, 0.4f,  // bottom right
+        -0.3f, -0.15, 0.0f,  1.0f, 0.4f, 0.4f,  // bottom left
+        -0.2f, -0.3f, 0.0f,  1.0f, 0.4f, 0.4f,   // top
+        //triangle 5
+        // positions         // colors
+        -0.2f, 0.0f, 0.0f,  1.0f, 0.4f, 0.4f, // bottom right
+        -0.2f, -0.3f, 0.0f,  1.0f, 0.4f, 0.4f,  // bottom left
+        -0.1f, -0.15f, 0.0f,  1.0f, 0.4f, 0.4f,  // top
+        //triangle 6
+        // positions         // colors
+        -0.2f, -0.3f, 0.0f,   1.0f, 0.6f, 0.2f,  // bottom right
+        -0.1f, -0.15f, 0.0f,  1.0f, 0.6f, 0.2f,  // bottom left
+        0.0f, -0.3f, 0.0f,  1.0f, 0.6f, 0.2f,   // top
+        //triangle 7
+        // positions         // colors
+        -0.2f, -0.3f, 0.0f,  0.6f, 0.8f, 0.2f,  // bottom right
+        -0.1f, -0.45f, 0.0f,  0.6f, 0.8f, 0.2f,  // bottom left
+        0.05f, -0.3f, 0.0f,  0.6f, 0.8f, 0.2f,   // top
+        //triangle 8
+        // positions         // colors
+        -0.1f, -0.45f, 0.0f,  0.4f, 0.4f, 0.8f,  // bottom right
+        0.05f, -0.3f, 0.0f,  0.4f, 0.4f, 0.8f,  // bottom left
+        0.05f, -0.45f, 0.0f,  0.4f, 0.4f, 0.8f,   // top
+        //triangle 9
+        // positions         // colors
+        0.05f, -0.3f, 0.0f,  0.4f, 0.4f, 0.8f,  // bottom right
+        0.05f, -0.45f, 0.0f,  0.4f, 0.4f, 0.8f,  // bottom left
+        0.2f, -0.3f, 0.0f,  0.4f, 0.4f, 0.8f,   // top
+        
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    while (!glfwWindowShouldClose(window))
     {
-        // Render here!
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap front and back buffers
-        glfwSwapBuffers(window);
+        //Define uniform value
+//        float timeValue = glfwGetTime()*3;
+//        float greenValue = sin(timeValue) / 2.0f + 0.5f;
 
-        // Poll for and process events
+        // Usamos el shader class
+        ourShader.use();
+        //Send uniform value
+//        ourShader.setFloat("GreenValue", greenValue);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 27);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width
-    // and height will be significantly larger than specified on retina displays
     glViewport(0, 0, width, height);
 }
