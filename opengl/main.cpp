d84f0fd00be2d5b9acdd3ee749911b42bb43639a
Juan José Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-08-09 12:10:34 -0500

    exam IB

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -16,8 +16,6 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-float visibilyFactor = 0.5f;
-
 int main()
 {
     glfwInit();
@@ -29,7 +27,7 @@ int main()
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     #endif
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Juan Jaramillo", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "1722951165 - Juan Jaramillo", NULL, NULL);
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
@@ -40,7 +38,7 @@ int main()
     }
 
     Shader ourShader("shaders/practica_shader_class.vs", "shaders/practica_shader_class.fs");
-    Shader ourShader2("shaders/practica_shader_class-2.vs", "shaders/practica_shader_class-2.fs");
+    Shader ourShader_2("shaders/practica_shader_class-2.vs", "shaders/practica_shader_class-2.fs");
     
     //Iniciamos los vertices
     float vertices_1[] = {
@@ -61,7 +59,7 @@ int main()
         0.3f, -0.25f, 0.0f,     1.0f, 0.8f, 0.0f,       0.8f, 0.25f,     // J
     };
     
-    fload vertices_2[] = {
+    float vertices_2[] = {
         //triangle 4
         // positions            // colors               // texture position
         -0.2f, 0.0f, 0.0f,      1.0f, 0.4f, 0.4f,       0.3f, 0.5f,     // E
@@ -95,14 +93,29 @@ int main()
     };
 
     unsigned int VBOs[2], VAOs[2];
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
+    glGenVertexArrays(2, VAOs);
+    glGenBuffers(2, VBOs);
     
-    glBindVertexArray(VAO);
+    //setup group 1
+    glBindVertexArray(VAOs[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    
+    glEnableVertexAttribArray(2);
+    
+    //setup group 2
+    glBindVertexArray(VAOs[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_2), vertices_2, GL_STATIC_DRAW);
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -115,24 +128,17 @@ int main()
     
     glEnableVertexAttribArray(2);
 
-    // load and create a texture
-    // -------------------------
-    unsigned int texture1, texture2;
     // texture 1
-    // ---------
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // note that we set the container wrapping method to GL_CLAMP_TO_EDGE
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // set texture filtering to nearest neighbor to clearly see the texels/pixels
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    // load image, create texture and generate mipmaps
+    unsigned int texture_1;
+    glGenTextures(1, &texture_1);
+    glBindTexture(GL_TEXTURE_2D, texture_1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    unsigned char* data = stbi_load("textures/paisaje.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("textures/texture1.jpg", &width, &height, &nrChannels, 0);
+
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -140,48 +146,31 @@ int main()
     }
     else
     {
-        std::cout << "Failed to load texture" << std::endl;
+        std::cout << "Failed to load texture 1" << std::endl;
     }
     stbi_image_free(data);
-
+    
     // texture 2
-   // ---------
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);    // set texture wrapping to GL_REPEAT (default wrapping method)
+    unsigned int texture_2;
+    glGenTextures(1, &texture_2);
+    glBindTexture(GL_TEXTURE_2D, texture_2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-    data = stbi_load("textures/paisaje-2.jpg", &width, &height, &nrChannels, 0);
+    int width_2, height_2, nrChannels_2;
+    unsigned char* data_2 = stbi_load("textures/texture2.jpg", &width_2, &height_2, &nrChannels_2, 0);
+
     if (data)
     {
-        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
     else
     {
-        std::cout << "Failed to load texture" << std::endl;
+        std::cout << "Failed to load texture 2" << std::endl;
     }
-    stbi_image_free(data);
-
-    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
-    // -------------------------------------------------------------------------------------------
-    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
-    // either set it manually like so:
-    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
-    // or set it via the texture class
-    ourShader.setInt("texture2", 1);
-
-
-    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
-    // -------------------------------------------------------------------------------------------
-    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
-    // either set it manually like so:
-    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+    stbi_image_free(data_2);
     
     while (!glfwWindowShouldClose(window))
     {
@@ -189,32 +178,36 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        // bind texture
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-        
-        ourShader.setFloat("visible", visibilyFactor);
+        // bind texture 1
+        glBindTexture(GL_TEXTURE_2D, texture_1);
 
-        //Define uniform value
-//        float timeValue = glfwGetTime()*3;
-//        float greenValue = sin(timeValue) / 2.0f + 0.5f;
-
-        // Usamos el shader class
+        // use shaders_1
         ourShader.use();
+        //Define uniform value
+        float timeValue = glfwGetTime()*3;
+        float dinamicValue = sin(timeValue) / 2.0f + 0.5f;
         //Send uniform value
-//        ourShader.setFloat("GreenValue", greenValue);
+        ourShader.setFloat("dinamicValue", dinamicValue);
 
-        glBindVertexArray(VAO);
+        glBindVertexArray(VAOs[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 27);
+        
+        //bind texture 2
+        glBindTexture(GL_TEXTURE_2D, texture_2);
+        //use shaders_2
+        ourShader_2.use();
+        //Send uniform value
+        ourShader.setFloat("dinamicValue", dinamicValue);
+
+        glBindVertexArray(VAOs[1]);
         glDrawArrays(GL_TRIANGLES, 0, 27);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, VAOs);
+    glDeleteBuffers(1, VBOs);
 
     glfwTerminate();
     return 0;
--- opengl/shaders/practica_shader_class.fs
+++ opengl/shaders/practica_shader_class.fs
@@ -5,13 +5,9 @@ in vec3 ourColor;
 in vec2 TexCoord;
 
 uniform sampler2D texture1;
-uniform sampler2D texture2;
-uniform float visible;
+uniform float dinamicValue;
 
 void main()
 {
-//    FragColor = mix(texture(texture1, vec2(TexCoord.x, 1 - TexCoord.y)), vec4(ourColor, 1.0),0.5);
-//    FragColor = texture(texture1,  vec2(TexCoord.x, 1 - TexCoord.y));
-//    FragColor = vec4(ourColor, 1.0f);
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
+    FragColor = mix(texture(texture1, vec2(TexCoord.x, 1 - TexCoord.y)), vec4(ourColor, 1.0),dinamicValue);
 }
