5f4e0667fd3f7af5cd0befb742887458dc315a3e
Juan José Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-08-16 18:31:07 -0500

    finish task

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -1,23 +1,29 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <shader_s.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-#include <iostream>
-#include <filesystem>
-#include <math.h>
+#include <learnopengl/shader_s.h>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <learnopengl/stb_image.h>
+
+#include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+void processInput(GLFWwindow *window);
 
+// settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
+float scale = 1;
+float angle = 0;
 int main()
 {
+    // glfw: initialize and configure
+    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -27,199 +33,191 @@ int main()
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     #endif
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "1722951165 - Juan Jaramillo", NULL, NULL);
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Juan Jaramillo", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    Shader ourShader("shaders/practica_shader_class.vs", "shaders/practica_shader_class.fs");
-    Shader ourShader_2("shaders/practica_shader_class-2.vs", "shaders/practica_shader_class-2.fs");
-    
-    //Iniciamos los vertices
-    float vertices_1[] = {
-        //triangle 1
-        // positions            // colors               // texture position
-        0.0f, 0.5f, 0.0f,       0.2f, 0.6f, 1.0f,       0.5f, 1.0f,     // A
-        -0.2f, 0.3f, 0.0f,      0.2f, 0.6f, 1.0f,       0.3f, 0.8f,     // B
-        0.0f, 0.3f, 0.0f,       0.2f, 0.6f, 1.0f,       0.5f, 0.8f,     // C
-        //triangle 2
-        // positions            // colors               // texture position
-        0.0f, 0.3f, 0.0f,       0.2f, 0.8f, 0.4f,       0.5f, 0.8f,     // C
-        -0.2f, 0.0f, 0.0f,      0.2f, 0.8f, 0.4f,       0.3f, 0.5f,     // E
-        0.0f, -0.3f, 0.0f,      0.2f, 0.8f, 0.4f,       0.5f, 0.2f,     // K
-        //triangle 3
-        // positions            // colors               // texture position
-        0.0f, 0.2f, 0.0f,       1.0f, 0.8f, 0.0f,       0.5f, 0.7f,     // D
-        0.0f, -0.25f, 0.0f,     1.0f, 0.8f, 0.0f,       0.5f, 0.25f,     // I
-        0.3f, -0.25f, 0.0f,     1.0f, 0.8f, 0.0f,       0.8f, 0.25f,     // J
-    };
-    
-    float vertices_2[] = {
-        //triangle 4
-        // positions            // colors               // texture position
-        -0.2f, 0.0f, 0.0f,      1.0f, 0.4f, 0.4f,       0.3f, 0.5f,     // E
-        -0.3f, -0.15, 0.0f,     1.0f, 0.4f, 0.4f,       0.2f, 0.35f,    // F
-        -0.2f, -0.3f, 0.0f,     1.0f, 0.4f, 0.4f,       0.3f, 0.2f,     // G
-        //triangle 5
-        // positions            // colors               // texture position
-        -0.2f, 0.0f, 0.0f,      1.0f, 0.4f, 0.4f,       0.3f, 0.5f,     // E
-        -0.2f, -0.3f, 0.0f,     1.0f, 0.4f, 0.4f,       0.3f, 0.2f,     // G
-        -0.1f, -0.15f, 0.0f,    1.0f, 0.4f, 0.4f,       0.4f, 0.35f,    // H
-        //triangle 6
-        // positions            // colors               // texture position
-        -0.2f, -0.3f, 0.0f,     1.0f, 0.6f, 0.2f,       0.3f, 0.2f,     // G
-        -0.1f, -0.15f, 0.0f,    1.0f, 0.6f, 0.2f,       0.4f, 0.35f,    // H
-        0.0f, -0.3f, 0.0f,      1.0f, 0.6f, 0.2f,       0.5f, 0.2f,     // K
-        //triangle 7
-        // positions             // colors              // texture position
-        -0.2f, -0.3f, 0.0f,     0.6f, 0.8f, 0.2f,       0.3f, 0.2f,     // G
-        -0.1f, -0.45f, 0.0f,    0.6f, 0.8f, 0.2f,       0.4f, 0.05f,    // L
-        0.05f, -0.3f, 0.0f,     0.6f, 0.8f, 0.2f,       0.55f, 0.2f,     // O
-        //triangle 8
-        // positions            // colors               // texture position
-        -0.1f, -0.45f, 0.0f,    0.4f, 0.4f, 0.8f,       0.4f, 0.05f,    // L
-        0.05f, -0.3f, 0.0f,     0.4f, 0.4f, 0.8f,       0.55f, 0.2f,    // O
-        0.05f, -0.45f, 0.0f,    0.4f, 0.4f, 0.8f,       0.55f, 0.05f,   // M
-        //triangle 9
-        // positions            // colors               // texture position
-        0.05f, -0.3f, 0.0f,     0.4f, 0.4f, 0.8f,       0.55f, 0.2f,    // O
-        0.05f, -0.45f, 0.0f,    0.4f, 0.4f, 0.8f,       0.55f, 0.05f,   // M
-        0.2f, -0.3f, 0.0f,      0.4f, 0.4f, 0.8f,       0.7f, 0.2f,      // N
-    };
+    // build and compile our shader zprogram
+    // ------------------------------------
+    Shader ourShader("shaders/shader_exercise10.vs", "shaders/shader_exercise10.fs");
 
-    unsigned int VBOs[2], VAOs[2];
-    glGenVertexArrays(2, VAOs);
-    glGenBuffers(2, VBOs);
-    
-    //setup group 1
-    glBindVertexArray(VAOs[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_1), vertices_1, GL_STATIC_DRAW);
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        // positions          // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    // texture attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    
-    glEnableVertexAttribArray(2);
-    
-    //setup group 2
-    glBindVertexArray(VAOs[1]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_2), vertices_2, GL_STATIC_DRAW);
 
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    // texture attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    
-    glEnableVertexAttribArray(2);
 
+    // load and create a texture
+    // -------------------------
+    unsigned int texture1, texture2;
     // texture 1
-    unsigned int texture_1;
-    glGenTextures(1, &texture_1);
-    glBindTexture(GL_TEXTURE_2D, texture_1);
+    // ---------
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // set the texture wrapping parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
     int width, height, nrChannels;
-    unsigned char* data = stbi_load("textures/texture1.jpg", &width, &height, &nrChannels, 0);
-
+    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+    unsigned char* data = stbi_load("textures/texture1.jpg", &width, &height, &nrChannels, 0);    if (data)
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture 1" << std::endl;
     }
     stbi_image_free(data);
-    
     // texture 2
-    unsigned int texture_2;
-    glGenTextures(1, &texture_2);
-    glBindTexture(GL_TEXTURE_2D, texture_2);
+    // ---------
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // set the texture wrapping parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    int width_2, height_2, nrChannels_2;
-    unsigned char* data_2 = stbi_load("textures/texture2.jpg", &width_2, &height_2, &nrChannels_2, 0);
-
+    // load image, create texture and generate mipmaps
+    data = stbi_load("textures/texture2.jpg", &width, &height, &nrChannels, 0);    if (data)
     if (data)
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
-    else
-    {
-        std::cout << "Failed to load texture 2" << std::endl;
-    }
-    stbi_image_free(data_2);
+    stbi_image_free(data);
+
+    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+    // -------------------------------------------------------------------------------------------
+    ourShader.use();
+    ourShader.setInt("texture1", 0);
+    ourShader.setInt("texture2", 1);
     
+    // render loop
+    // -----------
     while (!glfwWindowShouldClose(window))
     {
+        // input
+        // -----
         processInput(window);
+
+        // render
+        // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        glm::mat4 transform = glm::mat4(1.0f);
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        transform = glm::mat4(1.0f); //RESET THE TRANSFORMATION MATRIX
+        transform = glm::translate(transform, glm::vec3(sin(glfwGetTime()), cos(glfwGetTime()), 0.0f));
         
-        // bind texture 1
-        glBindTexture(GL_TEXTURE_2D, texture_1);
-
-        // use shaders_1
-        ourShader.use();
-        //Define uniform value
-        float timeValue = glfwGetTime()*3;
-        float dinamicValue = sin(timeValue) / 2.0f + 0.5f;
-        //Send uniform value
-        ourShader.setFloat("dinamicValue", dinamicValue);
-
-        glBindVertexArray(VAOs[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 27);
+        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+                scale = scale - 0.1;
         
-        //bind texture 2
-        glBindTexture(GL_TEXTURE_2D, texture_2);
-        //use shaders_2
-        ourShader_2.use();
-        //Send uniform value
-        ourShader.setFloat("dinamicValue", dinamicValue);
+        
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+                scale = scale + 0.1;
+        
+        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+                angle = angle - 0.1;
+        
+        
+        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+            angle = angle + 0.1;
+        
+        transform = glm::rotate(transform, (float)angle, glm::vec3(0.0f, 1.0f, 0.0f));
+        
+        transform = glm::scale(transform, glm::vec3(scale, scale, scale));
 
-        glBindVertexArray(VAOs[1]);
-        glDrawArrays(GL_TRIANGLES, 0, 27);
+        // get matrix's uniform location and set matrix
+        //another option
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &transform[0][0]);
 
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, VAOs);
-    glDeleteBuffers(1, VBOs);
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
-void processInput(GLFWwindow* window)
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
