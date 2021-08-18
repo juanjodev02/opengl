f9de0f8a69d719362fcab5d752afdef4d4118b0f
Juan José Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-08-17 21:32:39 -0500

    task 1

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -5,7 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include <learnopengl/shader_s.h>
+#include <learnopengl/shader.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <learnopengl/stb_image.h>
@@ -18,8 +18,7 @@ void processInput(GLFWwindow *window);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-float scale = 1;
-float angle = 0;
+
 int main()
 {
     // glfw: initialize and configure
@@ -29,9 +28,9 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    #ifdef __APPLE__
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    #endif
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     // glfw window creation
     // --------------------
@@ -107,11 +106,15 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char* data = stbi_load("textures/texture1.jpg", &width, &height, &nrChannels, 0);    if (data)
+    unsigned char* data = stbi_load("textures/container.jpg", &width, &height, &nrChannels, 0);    if (data)
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
     }
     stbi_image_free(data);
     // texture 2
@@ -125,12 +128,16 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load("textures/texture2.jpg", &width, &height, &nrChannels, 0);    if (data)
+    data = stbi_load("textures/awesomeface.png", &width, &height, &nrChannels, 0);    if (data)
     if (data)
     {
         // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
     }
     stbi_image_free(data);
 
@@ -139,7 +146,8 @@ int main()
     ourShader.use();
     ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
-    
+
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -152,38 +160,39 @@ int main()
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // bind textures on corresponding texture units
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        glm::mat4 transform = glm::mat4(1.0f);
-        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-        transform = glm::mat4(1.0f); //RESET THE TRANSFORMATION MATRIX
-        transform = glm::translate(transform, glm::vec3(sin(glfwGetTime()), cos(glfwGetTime()), 0.0f));
-        
-        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-                scale = scale - 0.1;
-        
-        
-        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-                scale = scale + 0.1;
-        
-        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-                angle = angle - 0.1;
-        
-        
-        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-            angle = angle + 0.1;
-        
-        transform = glm::rotate(transform, (float)angle, glm::vec3(0.0f, 1.0f, 0.0f));
-        
-        transform = glm::scale(transform, glm::vec3(scale, scale, scale));
+        // activate shader
+        ourShader.use();
 
-        // get matrix's uniform location and set matrix
-        //another option
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &transform[0][0]);
+        //create transformations
+        glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+        glm::mat4 view          = glm::mat4(1.0f);
+        glm::mat4 projection    = glm::mat4(1.0f);
 
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+        //retrieve teh matrix uniform locations
+        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+        unsigned int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+
+        // pass them to the shaders (3 different ways)
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, &projection[0][0]);
+        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+//        ourShader.setMat4("projection", projection);
+
+        // render container
+        glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
@@ -221,3 +230,4 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
+
