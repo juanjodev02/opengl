ea04e03f5d936b1ba01db6f232570c51ffc680a9
Juan José Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-08-18 19:04:27 -0500

    simple ligth

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -18,20 +18,28 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
+// settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-// Camara Settings
+// camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
 
-float deltaTime = 0.0f;
+// timing
+float deltaTime = 0.0f;    // time between current frame and last frame
 float lastFrame = 0.0f;
+
+//Exercise 13
+//lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main()
 {
+    // glfw: initialize and configure
+    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -40,7 +48,10 @@ int main()
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Juan Jaramillo", NULL, NULL);
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Exercise 13 Task 1", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -49,219 +60,178 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    // Config mouse envents
     glfwSetCursorPosCallback(window, mouse_callback);
-    // Config scroll events
     glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    // Enable z buffer
+
+
+    // configure global opengl state
+    //Exercise 11 Task 3
+    // -----------------------------
     glEnable(GL_DEPTH_TEST);
-    
-    // Load Shaders
-    Shader ourShader("shaders/shader_exercise11.vs", "shaders/shader_exercise11.fs");
-    
-    // Setup position vectors
+
+    //Exercise 13 Task 1
+    // build and compile our shader zprogram
+    // ------------------------------------
+    Shader lightingShader("shaders/shader_exercise13_colors.vs", "shaders/shader_exercise13_colors.fs");
+    Shader lightCubeShader("shaders/shader_exercise13_lightcube.vs", "shaders/shader_exercise13_lightcube.fs");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
     float vertices[] = {
-    // positions          // texture coords
-    //CARA ATRÁS
-    -0.5f, -0.5f, -0.5f,  0.725f, 0.326f,
-     0.5f, -0.5f, -0.5f,  0.933f, 0.326f,
-     0.5f,  0.5f, -0.5f,  0.933f, 0.58f,
-     0.5f,  0.5f, -0.5f,  0.933f, 0.58f,
-    -0.5f,  0.5f, -0.5f,  0.725f, 0.58f,
-    -0.5f, -0.5f, -0.5f,  0.725f, 0.326f,
+         -0.5f, -0.5f, -0.5f,
+          0.5f, -0.5f, -0.5f,
+          0.5f,  0.5f, -0.5f,
+          0.5f,  0.5f, -0.5f,
+         -0.5f,  0.5f, -0.5f,
+         -0.5f, -0.5f, -0.5f,
 
-    //CARA FRONTAL
-    -0.5f, -0.5f,  0.5f,  0.3f, 0.31f,
-     0.5f, -0.5f,  0.5f,  0.51f, 0.31f,
-     0.5f,  0.5f,  0.5f,  0.51f, 0.58f,
-     0.5f,  0.5f,  0.5f,  0.51f, 0.58f,
-    -0.5f,  0.5f,  0.5f,  0.3f, 0.58f,
-    -0.5f, -0.5f,  0.5f,  0.3f, 0.31f,
+         -0.5f, -0.5f,  0.5f,
+          0.5f, -0.5f,  0.5f,
+          0.5f,  0.5f,  0.5f,
+          0.5f,  0.5f,  0.5f,
+         -0.5f,  0.5f,  0.5f,
+         -0.5f, -0.5f,  0.5f,
 
-    //CARA IZQUIERDA
-    -0.5f,  0.5f,  0.5f,  0.085f, 0.58f,
-    -0.5f,  0.5f, -0.5f,  0.295f, 0.58f,
-    -0.5f, -0.5f, -0.5f,  0.295f, 0.33f,
-    -0.5f, -0.5f, -0.5f,  0.295f, 0.33f,
-    -0.5f, -0.5f,  0.5f,  0.085f, 0.33f,
-    -0.5f,  0.5f,  0.5f,  0.085f, 0.58f,
+         -0.5f,  0.5f,  0.5f,
+         -0.5f,  0.5f, -0.5f,
+         -0.5f, -0.5f, -0.5f,
+         -0.5f, -0.5f, -0.5f,
+         -0.5f, -0.5f,  0.5f,
+         -0.5f,  0.5f,  0.5f,
 
-    //CARA DERECHA
-     0.5f,  0.5f,  0.5f,  0.51f, 0.58f,
-     0.5f,  0.5f, -0.5f,  0.72f, 0.58f,
-     0.5f, -0.5f, -0.5f,  0.72f, 0.326f,
-     0.5f, -0.5f, -0.5f,  0.72f, 0.326f,
-     0.5f, -0.5f,  0.5f,  0.51f, 0.326f,
-     0.5f,  0.5f,  0.5f,  0.51f, 0.58f,
+          0.5f,  0.5f,  0.5f,
+          0.5f,  0.5f, -0.5f,
+          0.5f, -0.5f, -0.5f,
+          0.5f, -0.5f, -0.5f,
+          0.5f, -0.5f,  0.5f,
+          0.5f,  0.5f,  0.5f,
 
+         -0.5f, -0.5f, -0.5f,
+          0.5f, -0.5f, -0.5f,
+          0.5f, -0.5f,  0.5f,
+          0.5f, -0.5f,  0.5f,
+         -0.5f, -0.5f,  0.5f,
+         -0.5f, -0.5f, -0.5f,
 
-     //CARA ABAJO
-    -0.5f, -0.5f, -0.5f,  0.3f, 0.32f,
-     0.5f, -0.5f, -0.5f,  0.51f, 0.32f,
-     0.5f, -0.5f,  0.5f,  0.51f, 0.07f,
-     0.5f, -0.5f,  0.5f,  0.51f, 0.07f,
-    -0.5f, -0.5f,  0.5f,  0.3f, 0.07f,
-    -0.5f, -0.5f, -0.5f,  0.3f, 0.32f,
-
-    //CARA ARRIBA
-    -0.5f,  0.5f, -0.5f,  0.3f, 0.83f,
-     0.5f,  0.5f, -0.5f,  0.3f, 0.57f,
-     0.5f,  0.5f,  0.5f,  0.51f, 0.57f,
-     0.5f,  0.5f,  0.5f,  0.51f, 0.57f,
-    -0.5f,  0.5f,  0.5f,  0.51f, 0.83f,
-    -0.5f,  0.5f, -0.5f,  0.3f, 0.83f
+         -0.5f,  0.5f, -0.5f,
+          0.5f,  0.5f, -0.5f,
+          0.5f,  0.5f,  0.5f,
+          0.5f,  0.5f,  0.5f,
+         -0.5f,  0.5f,  0.5f,
+         -0.5f,  0.5f, -0.5f,
     };
-
-    // Define world positions for each cubes
-    glm::vec3 cubePositions[] = {
-        glm::vec3(0.0f,  0.0f,  0.0f),
-        glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f,  2.0f, -2.5f)
-    };
-
-    // Define VBO and VAO
-    unsigned int VBO, VAO;
-    // Init VAO and VBO
-    glGenVertexArrays(1, &VAO);
+    
+    //Exercise 13 Task 1
+    // first, configure the cube's VAO (and VBO)
+    unsigned int VBO, cubeVAO;
+    glGenVertexArrays(1, &cubeVAO);
     glGenBuffers(1, &VBO);
- 
-    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+
+    glBindVertexArray(cubeVAO);
+    
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // Texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-
-    // Load Textures
-    unsigned int texture1, texture2;
-    // Texture 1
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char* data = stbi_load("textures/tnt.png", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        // Change config to use .png files
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-    // Texture 2
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    data = stbi_load("textures/fire.png", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-    ourShader.use();
-    ourShader.setInt("texture1", 0);
-    ourShader.setInt("texture2", 1);
-
-    // Render Loop
+    
+    // second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
+    unsigned int lightCubeVAO;
+    glGenVertexArrays(1, &lightCubeVAO);
+    glBindVertexArray(lightCubeVAO);
+    
+     // we only need to bind to the VBO (to link it with glVertexAttribPointer), no need to fill it; the VBO's data already contains all we need (it's already bound, but we do it again for educational purposes)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    
+    // render loop
+    // -----------
     while (!glfwWindowShouldClose(window))
     {
+        // per-frame time logic
+       // --------------------
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
+
+        // input
+        // -----
         processInput(window);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        
+                // render
+        // ------
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Use textures
-        glActiveTexture(GL_TEXTURE0);
-
-        glActiveTexture(GL_TEXTURE1);
         
+       // be sure to activate shader when setting uniforms/drawing objects
+        lightingShader.use();
+        lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
 
-        // Activate shaders
-        ourShader.use();
-        
-        // Define projection matrix
+        // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        ourShader.setMat4("projection", projection);
-        
-         // Define view matrix | Usage of camera class
         glm::mat4 view = camera.GetViewMatrix();
-        ourShader.setMat4("view", view);
+        lightingShader.setMat4("projection", projection);
+        lightingShader.setMat4("view", view);
 
-        // Use VAO
-        glBindVertexArray(VAO);
+        // world transformation
+        glm::mat4 model = glm::mat4(1.0f);
+        lightingShader.setMat4("model", model);
 
-        // Render one cube for each position vectors using first texture
-        for (unsigned int i = 0; i < 5; i++)
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            if (i % 2 == 0)
-                angle = glfwGetTime() * 25.0f;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            ourShader.setMat4("model", model);
-            // Use first texture
-            glBindTexture(GL_TEXTURE_2D, texture1);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-        
-        for (unsigned int i = 5; i < 8; i++)
-        {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            if (i % 2 == 0)
-                angle = glfwGetTime() * 25.0f;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            ourShader.setMat4("model", model);
-            // Use first texture
-            glBindTexture(GL_TEXTURE_2D, texture2);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+        // render the cube
+        glBindVertexArray(cubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
+        // also draw the lamp object
+        lightCubeShader.use();
+        lightCubeShader.setMat4("projection", projection);
+        lightCubeShader.setMat4("view", view);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+        lightCubeShader.setMat4("model", model);
+
+        glBindVertexArray(lightCubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
+        
     }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
     
+        // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &cubeVAO);
+    glDeleteVertexArrays(1, &lightCubeVAO);
+    glDeleteBuffers(1, &VBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
+    
 }
 
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -275,17 +245,16 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
-
-    //If I want to stay in ground level (xz plane)
-//    camera.Position.y = 0.0f;
-    
 }
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
 
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
     if (firstMouse)
@@ -296,13 +265,15 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     }
 
     float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
     lastX = xpos;
     lastY = ypos;
 
     camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(yoffset);
